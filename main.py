f2fe31236aa50b0eb85ed6f847b3d8f06df32488
JakubG <jakub.grela.pl@gmail.com>
2024-04-24 16:09:57 +0200

    Refactor code

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import re
+import os
+
+
 class Invoice:
     def __init__(self, file):
         self.file = file
@@ -19,6 +22,7 @@ class InvoiceHandler:
         self.header = ""
         self.transactions = []
         self.footer = ""
+        self.currency = ["PLN", "EUR", "USD"]
 
     def read_invoice_data(self):
         self.lines = self.read_invoice()
@@ -71,13 +75,14 @@ class InvoiceHandler:
 
     def read_transaction(self, transaction_number):
         searched_transaction = ""
-        transactions = self.read_transactions()
-        for transaction in transactions:
-            searched_transaction = re.search("02(\d\d\d\d\d\d)0000000(\d\d\d\d\d)([a-zA-Z]+)" ,transaction)
-            print(searched_transaction.group(1))
-            if transaction_number == searched_transaction.group(1).lstrip("0"):
-                break
-        return searched_transaction.string
+        # transactions = self.read_transactions()
+        # for transaction in transactions:
+        #     searched_transaction = re.search("02(\d\d\d\d\d\d)0000000(\d\d\d\d\d)([a-zA-Z]+)" ,transaction)
+        #     print(searched_transaction.group(1))
+        #     if transaction_number == searched_transaction.group(1).lstrip("0"):
+        #         break
+        index = int(transaction_number) - 1
+        return self.transactions[index]
 
     def read_footer(self):
         return self.footer
@@ -108,7 +113,6 @@ class InvoiceHandler:
         transaction_data = re.search("02(\d\d\d\d\d\d)0000000(\d\d\d\d\d)([a-zA-Z]+)" ,transaction)
         counter = transaction_data.group(1)
         current_amount = transaction_data.group(2)
-        print(type(current_amount))
         current_currency = transaction_data.group(3)
         current_reserved = ""
         for key, value in modyfication.items():
@@ -135,16 +139,6 @@ class InvoiceHandler:
             amount += int(transaction_data.group(2))
         return amount
 
-    # def update_invoice(self, old_value, new_value):
-    #     self.write_data_to_footer()
-    #     with open(self.file, "w") as f:
-    #         for line in self.lines:
-    #             if line.strip() == old_value.strip():
-    #                 f.write(new_value)
-    #                 f.write("\n")
-    #             else:
-    #                 f.write(line)
-
     def read_value_of_specific_field(self):
         field = input("What kind of field? [(H)eader : (T)ransactions : (F)ooter]\n")
         if field == "H":
@@ -168,7 +162,6 @@ class InvoiceHandler:
         elif field == "T":
             transaction_number = input("What transaction are you interested in? [1..20000]\n")
             transaction = self.read_transaction(transaction_number)
-            print(transaction)
             if transaction:
                 value = input("What kind of Value? [(C)ounter : (A)mount : (Cu)rrency : (R)eserved]\n")
                 if value == "C":
@@ -204,160 +197,204 @@ class InvoiceHandler:
             print("Wrong input")
 
 
-
-
-
-class GUI:
+class APP:
     def __init__(self):
         self.program()
+        self.invoice_handler = None
+
+    def create_new_invoice(self):
+        print("--------------------------------")
+        print("------Creating new invoice------")
+        print("--------------------------------")
+        invoice = Invoice(input("Pass invoice name[.txt]\n"))
+        self.invoice_handler = InvoiceHandler(invoice.file)
+        name = input("Name?\n")
+        surname = input("Surname?\n")
+        patronymic = input("Patronymic?\n")
+        address = input("Address?\n")
+        if not name or not surname or not patronymic:
+            print("Invalid input")
+            return 0
+        self.invoice_handler.write_data_to_header(name, surname, patronymic, address)
+        print("--------------------------------")
+        add_transaction = self.ask_for_transaction()
+        while add_transaction == "y":
+            amount = input("Amount?\n")
+            currency = input("Currency?\n")
+            if not amount.isdigit() or currency not in self.invoice_handler.currency:
+                print("Invalid amount or currency. Transaction not added")
+                add_transaction = self.ask_for_transaction()
+                continue
+            self.invoice_handler.write_data_to_transaction(amount, currency)
+            add_transaction = self.ask_for_transaction()
+        self.invoice_handler.write_data_to_footer()
+        self.invoice_handler.commit()
+        print("!!!Invoice created!!!")
+
+    def ask_for_transaction(self):
+        add_transaction = input("Do you want to add transaction? [y/n]\n")
+        add_transaction = add_transaction.lower()
+        return add_transaction
+
+    def print_invoice(self):
+        self.print_invoice_menu()
+        printing_option = input("\n")
+        if printing_option == "1":
+            lines = self.invoice_handler.read_invoice()
+            for line in lines:
+                print(line)
+        elif printing_option == "2":
+            print(self.invoice_handler.read_header())
+        elif printing_option == "3":
+            if self.invoice_handler.counter == 0:
+                print("There are no transactions added yet")
+                return 0
+            transaction_id = input("ID of transaction")
+            if int(transaction_id) not in range(self.invoice_handler.counter+1):
+                print("There is no transaction with this ID")
+                return 0
+            print(self.invoice_handler.read_transaction(transaction_id))
+        elif printing_option == "4":
+            transactions = self.invoice_handler.read_transactions()
+            for transaction in transactions:
+                print(transaction)
+        elif printing_option == "5":
+            print(self.invoice_handler.read_footer())
+        elif printing_option == "6":
+            self.invoice_handler.read_value_of_specific_field()
+        elif printing_option == "7":
+            print("Operation cancelled")
+        else:
+            print("Wrong input")
+
+    def modify_invoice(self):
+        self.print_modify_invoice_menu()
+        modify_command = input("\n")
+        if modify_command == "1":
+            print("!!!If you dont want to modify specific field just press enter!!!")
+            name = input("Name?\n")
+            surname = input("Surname?\n")
+            address = input("Address?\n")
+            patronymic = input("Patronymic?\n")
+            if not name or not surname or not patronymic:
+                print("Invalid input")
+                return 0
+            modification = dict()
+            if name:
+                modification["name"] = name
+            if surname:
+                modification["surname"] = surname
+            if address:
+                modification["address"] = address
+            if patronymic:
+                modification["patronymic"] = patronymic
+            self.invoice_handler.modify_header(modification)
+        elif modify_command == "2":
+            print("!!!If you dont want to modify specific field leave it empty!!!")
+            if self.invoice_handler.counter == 0:
+                print("There are no transactions added yet")
+                return 0
+            transaction_id = input("Transaction ID?")
+            if int(transaction_id) not in range(self.invoice_handler.counter+1):
+                print("There is no transaction with this ID")
+                return 0
+            amount = input("Amount?")
+            currency = input("Currency?")
+            if not amount.isdigit() or currency not in self.invoice_handler.currency:
+                print("Invalid amount or currency. Transaction not added")
+                return 0
+            modification = dict()
+            if amount:
+                modification["amount"] = amount
+            if currency:
+                modification["currency"] = currency
+            self.invoice_handler.modify_transaction(transaction_id, modification)
+        elif modify_command == "3":
+            print("Closing")
+        else:
+            print("Invalid command, closing.")
+
+    def open_invoice(self):
+        print("--------------------------------")
+        print("------  Opening  invoice  ------")
+        print("--------------------------------")
+        invoice = input("Pass name[.txt] of existing invoice\n")
+        if not os.path.exists(invoice):
+            print("This file does not exists")
+            return 0
+        self.invoice_handler = InvoiceHandler(invoice)
+        self.invoice_handler.read_invoice_data()
+        self.print_open_invoice_menu()
+        internal_command = input()
+        while internal_command != "4":
+            if internal_command == "1":
+                self.print_invoice()
+            elif internal_command == "2":
+                self.modify_invoice()
+            elif internal_command == "3":
+                amount = input("Amount?")
+                currency = input("Currency?")
+                if not amount.isdigit() or currency not in self.invoice_handler.currency:
+                    print("Invalid amount or currency. Transaction not added")
+                    self.print_open_invoice_menu()
+                    internal_command = input()
+                    continue
+                self.invoice_handler.add_transaction(amount, currency)
+            elif internal_command == "4":
+                break
+            else:
+                print("Wrong input")
+            self.print_open_invoice_menu()
+            internal_command = input()
+        print("Closing invoice")
 
     def program(self):
+        self.print_program_menu()
+        command = input()
+        while command != 3:
+            if command == "1":
+                self.create_new_invoice()
+            elif command == "2":
+                self.open_invoice()
+            elif command == "3":
+                print("Exiting program")
+                break
+            else:
+                print("Wrong command")
+            self.print_program_menu()
+            command = input()
+
+    def print_invoice_menu(self):
+        print("--------------------------------")
+        print("------  Printing invoice  ------")
+        print("--------------------------------")
+        print("What would you like to print?")
+        print("1. Invoice")
+        print("2. Header")
+        print("3. Transaction")
+        print("4. All transactions")
+        print("5. Footer")
+        print("6. Specific field")
+        print("7. Exit")
+
+    def print_modify_invoice_menu(self):
+        print("Which resource do you want to modify?")
+        print("1. Header")
+        print("2. Transaction")
+        print("3. Exit")
+
+    def print_open_invoice_menu(self):
+        print("1. Print invoice")
+        print("2. Modify invoice")
+        print("3. Add transaction")
+        print("4. Exit")
+
+    def print_program_menu(self):
         print("Hello, what would you like to do?")
         print("1. Create invoice")
         print("2. Operate on existing one")
         print("3. Exit\n")
-        command = input()
-        while command != 4:
-            if command == "1":
-                print("--------------------------------")
-                print("------Creating new invoice------")
-                print("--------------------------------")
-                invoice = Invoice(input("Pass invoice name[.txt]\n"))
-                invoice_handler = InvoiceHandler(invoice.file)
-                name = input("Name?")
-                surname = input("Surname?")
-                patronymic = input("Patronymic?")
-                address = input("Address?")
-                invoice_handler.write_data_to_header(name, surname, patronymic, address)
-                print("--------------------------------")
-                add_transaction = input("Do you want to add transaction? [y/n]\n")
-                add_transaction = add_transaction.lower()
-                while add_transaction == "y":
-                    amount = input("Amount?")
-                    currency = input("Currency?")
-                    invoice_handler.write_data_to_transaction(amount, currency)
-                    add_transaction = input("Do you want to add transaction? [y/n]\n")
-                    add_transaction = add_transaction.lower()
-                invoice_handler.write_data_to_footer()
-                invoice_handler.commit()
-                print("!!!Invoice created!!!")
-            elif command == "2":
-                print("--------------------------------")
-                print("------  Opening  invoice  ------")
-                print("--------------------------------")
-                invoice = input("Pass name[.txt] of existing invoice\n")
-                invoice_handler = InvoiceHandler(invoice)
-                invoice_handler.read_invoice_data()
-                print("1. Print invoice")
-                print("2. Modify invoice")
-                print("3. Add transaction")
-                print("4. Exit")
-                internal_command = input()
-                while (internal_command != "4"):
-                    if internal_command == "1":
-                        print("--------------------------------")
-                        print("------  Printing invoice  ------")
-                        print("--------------------------------")
 
-                        print("What would you like to print?")
-                        print("1. Invoice")
-                        print("2. Header")
-                        print("3. Transaction")
-                        print("4. All transactions")
-                        print("5. Footer")
-                        print("6. Specific field")
-                        print("7. Exit")
-                        printing_option = input()
-                        if printing_option == "1":
-                            lines = invoice_handler.read_invoice()
-                            for line in lines:
-                                print(line)
-                        elif printing_option == "2":
-                            print(invoice_handler.read_header())
-                        elif printing_option == "3":
-                            transaction_id = input("ID of transaction")
-                            print(invoice_handler.read_transaction(transaction_id))
-                        elif printing_option == "4":
-                            transactions = invoice_handler.read_transactions()
-                            for transaction in transactions:
-                                print(transaction)
-                        elif printing_option == "5":
-                            print(invoice_handler.read_footer())
-                        elif printing_option == "6":
-                            invoice_handler.read_value_of_specific_field()
-                        elif printing_option == "7":
-                            print("Operation cancelled")
-                        else:
-                            print("Wrong input")
-                    elif internal_command == "2":
-                        print("Which resource do you want to modify?")
-                        print("1. Header")
-                        print("2. Transaction")
-                        print("3. Exit")
-                        modify_command = input("\n")
-                        if modify_command == "1":
-                            print("!!!If you dont want to modify specific field just press enter!!!")
-                            name = input("Name?\n")
-                            surname = input("Surname?\n")
-                            address = input("Address?\n")
-                            patronymic = input("Patronymic?\n")
-                            modyfication = dict()
-                            if name:
-                                modyfication["name"] = name
-                            if surname:
-                                modyfication["surname"] = surname
-                            if address:
-                                modyfication["address"] = address
-                            if patronymic:
-                                modyfication["patronymic"] = patronymic
-                            invoice_handler.modify_header(modyfication)
-                        elif modify_command == "2":
-                            print("!!!If you dont want to modify specific field just press enter!!!")
-                            transaction_id = input("Transaction ID?")
-                            amount = input("Amount?")
-                            currency = input("Currency?")
-                            modyfication = dict()
-                            if amount:
-                                modyfication["amount"] = amount
-                            if currency:
-                                modyfication["currency"] = currency
-                            invoice_handler.modify_transaction(transaction_id, modyfication)
-                        elif modify_command == "3":
-                            print("Closing")
-                        else:
-                            print("Invalid command, closing.")
-                    elif internal_command == "3":
-                        amount = input("Amount?")
-                        currency = input("Currency?")
-                        invoice_handler.add_transaction(amount, currency)
-
-                    elif internal_command == "4":
-                        print("Closing invoice")
-                        break
-                    else:
-                        print("Wrong input")
-
-                    print("1. Print invoice")
-                    print("2. Modify invoice")
-                    print("3. Add transaction")
-                    print("4. Exit")
-                    internal_command = input()
-
-            elif command == "3":
-                print("Exiting program")
-                break
-
-            else:
-                print("Wrong command")
-            print("What would you like to do?")
-            print("1. Create invoice")
-            print("2. Operate on existing one")
-            print("3. Exit")
-            command = input()
 
 if __name__ == "__main__":
-    gui = GUI()
-    # invoice_handler = InvoiceHandler("new2.txt")
-    # print(invoice_handler.read_transactions())
-    # # print(invoice_handler.modify_header(name="Maciej", surname="GREEEEEEEEEEeeeeeEEEELA"))
-    # print(invoice_handler.modify_transaction(id="1", amount="600", currency="EUR"))
+    app = APP()
